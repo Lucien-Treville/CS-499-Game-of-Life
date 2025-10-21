a07c4f5b445dc22ac3486300b5652368c29d886b
Kjones23 <katanj123@yahoo.com>
2025-10-21 17:38:29 -0500

    ButtonChanges

--- Assets/LevelMenu.cs
+++ Assets/LevelMenu.cs
@@ -2,11 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class LevelMenu : MonoBehaviour
 {
+    // Called when a level button is clicked
     public void OpenLevel(int levelId)
     {
-        string levelName = "level " + levelId;
-        SceneManager.LoadScene(levelName);
+        string sceneName = GetSceneName(levelId);
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Invalid level ID selected: " + levelId);
+        }
+    }
+
+    // Maps level IDs to their correct scene names
+    private string GetSceneName(int levelId)
+    {
+        switch (levelId)
+        {
+            case 1:
+                return "Grasslands";
+            case 2:
+                return "Tundra";
+            case 3:
+                return "Swamp";
+            case 4:
+                return "Desert";
+
+            // "Build My Own Map" options — all go to Blank
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return "Blank";
+
+            default:
+                return null;
+        }
     }
 }
