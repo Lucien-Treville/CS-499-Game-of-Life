d2e02d7735551571f61b4c30dd894bdfe488dda1
ramseierjack <130418142+ramseierjack@users.noreply.github.com>
2025-12-01 20:59:17 -0600

    Graph Legend
    Added color legend to popgraph and adjusted positioning of report screen.

--- Assets/Scripts/ToggleSpawner.cs
+++ Assets/Scripts/ToggleSpawner.cs
@@ -38,12 +38,49 @@ public class ToggleSpawner : MonoBehaviour
         Toggle toggle = obj.GetComponent<Toggle>();
         toggle.isOn = true;
 
-        // Color the checkmark
-        if (PopulationManager.SpeciesColors.ContainsKey(speciesName))
+        // Color
+        // Color the checkmark, background, and the Toggle ColorBlock
+        if (PopulationManager.SpeciesColors.TryGetValue(speciesName, out Color c))
         {
-            Image check = toggle.graphic.GetComponent<Image>();
-            if (check != null)
-                check.color = PopulationManager.SpeciesColors[speciesName];
+            // declare up front so it's definitely assigned for later use
+            Image checkImg = null;
+
+            // assign if toggle.graphic is an Image
+            if (toggle.graphic is Image img)
+                checkImg = img;
+
+            // 1) Color the checkmark (if present)
+            if (checkImg != null)
+                checkImg.color = c;
+
+            // 2) Try to color a separate background image (common child name "Background")
+            Transform bgT = obj.transform.Find("Background") ?? obj.transform.Find("BackgroundImage");
+            if (bgT != null)
+            {
+                Image bgImg = bgT.GetComponent<Image>();
+                if (bgImg != null) bgImg.color = c * 0.9f;
+            }
+            else
+            {
+                // fallback: color the first Image child that is not the checkmark
+                Image[] imgs = obj.GetComponentsInChildren<Image>();
+                foreach (var imgChild in imgs)
+                {
+                    if (imgChild == checkImg) continue;
+                    imgChild.color = c * 0.9f;
+                    break;
+                }
+            }
+
+            // 3) Update the ColorBlock so transitions use the color
+            ColorBlock cb = toggle.colors;
+            cb.normalColor = c;
+            cb.highlightedColor = c * 1.1f;
+            cb.pressedColor = c * 0.9f;
+            cb.disabledColor = Color.gray;
+            cb.colorMultiplier = 1f;
+            cb.fadeDuration = 0.1f;
+            toggle.colors = cb;
         }
 
         toggle.onValueChanged.AddListener((isOn) =>
