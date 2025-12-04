afb19b6b2031c82c826acc11e4b9d5c043f0e588
Lucien-Treville <lucientreville@gmail.com>
2025-12-04 12:03:48 -0600

    fixed build mode file picker

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,10 +3,11 @@ using UnityEngine.SceneManagement;
 using System.IO;
 using TMPro;
 using UnityEngine.UI;
+using SFB;
 
-#if UNITY_EDITOR
-using UnityEditor;     // Only included when in Editor
-#endif
+// #if UNITY_EDITOR
+// using UnityEditor;     // Only included when in Editor
+// #endif
 
 public class SceneLoader : MonoBehaviour
 {
@@ -62,13 +63,37 @@ public class SceneLoader : MonoBehaviour
         }
     }
 
+    void LoadJsonFromPath(string path)
+    {
+        // 3. Read the text content
+        string fileContent = File.ReadAllText(path);
+        
+        Debug.Log($"Loaded: {path}");
+        
+        // 4. Pass to your Simulation Manager
+        // SimulationManager.Instance.LoadConfig(fileContent);
+    }
     public void UploadCustomTemplate()
     {
         WarningBox.gameObject.SetActive(false);
 
-#if UNITY_EDITOR
+// #if UNITY_EDITOR
         // --- EDITOR MODE: Use OpenFilePanel normally ---
-        string path = EditorUtility.OpenFilePanel("Select Custom Template JSON", "", "json");
+        // string path = EditorUtility.OpenFilePanel("Select Custom Template JSON", "", "json");
+        // 1. Open the native file dialog
+        // Arguments: Title, Directory, Extension Filter, Multiselect
+        var extensions = new [] {
+            new ExtensionFilter("JSON Files", "json"),
+            new ExtensionFilter("All Files", "*" ),
+        };
+
+        // This returns a string array of paths (even if just 1 file selected)
+        string[] paths = StandaloneFileBrowser.OpenFilePanel("Open JSON Template", "", extensions, false);
+        
+        string path = paths[0];
+        
+
+
 
         if (string.IsNullOrEmpty(path))
         {
@@ -94,14 +119,14 @@ public class SceneLoader : MonoBehaviour
         Debug.Log("Selected file: " + path);
         SceneManager.LoadScene("Grasslands");
 
-#else
+// #else
         // --- BUILD MODE: File picker is NOT allowed ---
-        Debug.LogWarning("Upload Custom Template only works inside the Unity Editor.");
+        // Debug.LogWarning("Upload Custom Template only works inside the Unity Editor.");
 
-        WarningText.text = "⚠ Custom file upload only works in the Unity Editor.\n" +
-                           "Build versions cannot open your file explorer.";
-        WarningBox.gameObject.SetActive(true);
-#endif
+        // WarningText.text = "⚠ Custom file upload only works in the Unity Editor.\n" +
+        //                    "Build versions cannot open your file explorer.";
+        // WarningBox.gameObject.SetActive(true);
+// #endif
     }
 
     public void UseDnDeditor()
